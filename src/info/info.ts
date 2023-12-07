b24748d5d4152ced4e1e131989b14416d40323b3
tpicaud <tom.picaud@alten.com>
2023-12-08 00:18:26 +0100

    add info

---
+++ src/info/info.ts
@@ -0,0 +1,107 @@
+export const info = {
+    baseUrl: "?",
+    name: "Tom Picaud",
+    jobDescription: "Blockchain addict",
+    about: `I am currently a student in software engineering, in my fifth and final year at ESIR, a French school based in Rennes.
+      I have studied a wide range of domains during my academic journey, including software modeling, cryptography, language theory, 
+      and full-stack web development. As a result, I have worked with technologies such as Java, C++, Python, Rust, as well as React,  
+      Angular, NestJS, Electron, and PostgreSQL.
+      In parallel, I delved into blockchain on my own, which has allowed me to acquire a strong understanding of the underlying technologies 
+      and the ecosystem. My goal is to continue learning new things, especially in the blockchain field, as I am convinced it has a 
+      great future ahead that I want to be a part of
+          `,
+  
+    experience: [
+      {
+        name: "ALTEN",
+        location: "Rennes, Brittany, France",
+        startDate: "Aug 2023",
+        endDate: "Present",
+        description: [
+          "• Capitalize on blockchain projects from the group's laboratories",
+          "• Help interns and new employees to get started with their blockchain environments",
+        ],
+      },
+      {
+        name: "IRISA - INRIA laboratory",
+        location: "Rennes, Brittany, France",
+        startDate: "Jun 2022",
+        endDate: "Jul 2022",
+        description: [
+          "• Development of a reliable broadcast algorithm to replace blockchain consensus",
+          "• Learning Rust, cryptographic signatures and networking",
+        ],
+      },
+  
+      {
+        name: "Table Tennis Châteaugiron",
+        location: "Châteaugiron, Brittany, France",
+        startDate: "Sep 2016",
+        endDate: "Feb 2023",
+        description: [
+          "• Coach of the club's senior/junior players",
+          "• Creation of customized sessions",
+          "• Leading a group of people",
+        ],
+      },
+    ],
+  
+    education: [
+      {
+        name: "Ecole Supérieure d'Ingénieurs de Rennes",
+        location: "Rennes, Brittany, France",
+        startDate: "2019",
+        endDate: "2024",
+        description: [
+          "• Engineer diploma in computer science (equivalent to a master's degree)",
+          "• International experience in Poland (Poznań University of Technology), 4 months"
+        ],
+      },
+      {
+        name: "Lycée Sévigné High School",
+        location: "Cesson-Sévigné, Brittany, France",
+        startDate: "2016",
+        endDate: "2019",
+        description: ["• Baccalauréat diploma, English section, with honors"],
+      },
+      {
+        name: "UFCV Bretagne",
+        location: "Brittany, France",
+        startDate: "2018",
+        endDate: "2019",
+        description: ["• Youth worker certificate, lifeguard qualification"],
+      },
+    ],
+  
+    skills: [
+      {
+        name: "Angular",
+        level: 80,
+      },
+      {
+        name: "React",
+        level: 60,
+      },
+      {
+        name: "Rust",
+        level: 60,
+      }
+    ],
+  
+    socialMedia: {
+      github: "https://github.com/tpicaud",
+      email: "tom.picaud@hotmail.com",
+      linkedin: "https://www.linkedin.com/in/tom-picaud-b9281222b/",
+    },
+  
+    projects: [
+      {
+        title: "Portfolio",
+        isFeatured: true,
+        thumbnail: "/assets/images/astro-portfolio.png",
+        githubUrl: "https://github.com/tpicaud/Portfolio",
+        //liveUrl: "",
+      },
+    ],
+  };
+  
