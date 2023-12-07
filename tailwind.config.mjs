42e39d66279dbd0560f6ec1a613e72995a920881
tpicaud <tom.picaud@alten.com>
2023-12-08 00:16:51 +0100

    add project colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				multicolor: 'linear-gradient(89.81deg, #9845e8 -1.72%, #33d2ff 54.05%, #dd5789 99.78%)',
+				dark: "#111111",
+				light: "#FFFFFF",
+				blue: "#33d2ff",
+				gray: "#C4C4C4",
+				"dark-gray": "#1A1A1A",
+			},
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+				outfit: ["Outfit", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 }
